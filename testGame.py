1ea668e020b9201df2160d6cd09e957265673e5b
John <Johngrntn@gmail.com>
2018-06-07 13:03:43 -0400

    Add files via upload
    This is just a simple console intro to a game that was easy and fun.

---
+++ testGame.py
@@ -0,0 +1,32 @@
+print("Greetings Commuter!")
+
+input("[Press Enter]")
+
+print("You are about to embark on an epic journey with stunning visuals, foreign planets, and grandiose orchestra music. I hope you are prepared.")
+
+input("[Press Enter]")
+
+print("This will be your command console. Think of it as your joystick, only with none of the working components of a traditional joystick. You can operate your ship from this terminal.")
+
+input("[Press Enter]")
+
+print("As your ship s AI, I would like to get to know you better. I am already well aware of your full birth name. What would you prefer being called onboard?")
+
+x = input("Name:")
+
+print("Excellent, " + x + ". Good to meet you.")
+
+input("[Press Enter]")
+
+print("Wow, making up nicknames is such a fun human activity. Why don/'t we come up with a nickname for me? What would you like to call me?")
+
+y = input("AI Name:")
+
+print(y + "? I love it!")
+
+input("[Press Enter]")
+
+print("Now. Let us get to colonizing planets in your name.")
+input("[Press Enter]")
+
+
